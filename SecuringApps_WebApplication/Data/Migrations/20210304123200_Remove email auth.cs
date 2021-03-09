901794e6d0dfaa814f45fa3dfe5fe366237b24ae
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-03-09 13:00:15 +0100

    inital commit

---
+++ SecuringApps_WebApplication/Data/Migrations/20210304123200_Remove email auth.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecuringApps_WebApplication.Data.Migrations
+{
+    public partial class Removeemailauth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

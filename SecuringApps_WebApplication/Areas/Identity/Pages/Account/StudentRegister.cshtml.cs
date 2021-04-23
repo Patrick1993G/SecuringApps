2083c09df78e8a58ffcdadf3d552618ba62907c7
Patrick1993G <49478732+Patrick1993G@users.noreply.github.com>
2021-04-23 09:45:17 +0200

    adding auto roles and auto email confirmation

--- SecuringApps_WebApplication/Areas/Identity/Pages/Account/StudentRegister.cshtml.cs
+++ SecuringApps_WebApplication/Areas/Identity/Pages/Account/StudentRegister.cshtml.cs
@@ -102,26 +102,23 @@ namespace SecuringApps_WebApplication.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email,EmailConfirmed = true };
                 var pwd = new Password();
                 var passResult = pwd.Next();
                 Input.Password = passResult;
                 var result = await _userManager.CreateAsync(user, Input.Password);
-
+               
                 if (result.Succeeded)
                 {
+                    //add a role
+                    var roleResult = await _userManager.AddToRoleAsync(user, "Student");
+                    if (!roleResult.Succeeded)
+                    {
+                        ModelState.AddModelError("",
+                        "Error while allocating role!");
+                    }
+
                     _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     //add student to student's table
                     StudentViewModel student = new StudentViewModel();
@@ -140,15 +137,10 @@ namespace SecuringApps_WebApplication.Areas.Identity.Pages.Account
                     //send details to student
                     string message = "Your email = "+ Input.Email + " and password= "+Input.Password;
                     SendEmail("smtp.live.com", 587, student.Email, "Student account details", message);
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                    
                     
                 }
                 foreach (var error in result.Errors)
